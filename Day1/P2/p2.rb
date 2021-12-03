a285289adb9ab77925905903106b718ab256bc49
Steve Pursey <Stephenpursey@gmail.com>
2021-12-03 15:00:32 -0800

    Clean up folder structure

---
+++ Day1/P2/p2.rb
@@ -0,0 +1,39 @@
+require 'time'
+
+INPUT_FILE_PATH = 'input/input.txt'
+
+start_time = Time.now
+
+inputs = []
+
+File.readlines(INPUT_FILE_PATH).each do |line|
+  inputs << line.to_i
+end
+
+prev_sum = 0
+cur_sum = 0
+counter = 0
+cur_measurements = []
+meas_increases = 0
+
+inputs.each_with_index do |meas, index|
+
+  next unless inputs[counter + 3]
+  prev_sum = inputs[counter] + inputs[counter + 1] + inputs[counter + 2]
+  cur_sum  = inputs[counter + 1] + inputs[counter + 2] + inputs[counter + 3]
+
+  if prev_sum < cur_sum
+    meas_increases += 1
+  end
+
+  prev_sum = 0
+  cur_sum  = 0
+
+  counter += 1
+end
+
+end_time = Time.now
+
+print "Finished in #{end_time - start_time} seconds.\n"
+
+print meas_increases
