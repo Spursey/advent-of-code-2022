d62f8559b99a648281d2d631ac8d2d60ba0ccc3d
Steve Pursey <Stephenpursey@gmail.com>
2022-12-02 17:09:29 -0800

    Add Day 1 project

---
+++ 2022/Day1/p1a.rb
@@ -0,0 +1,21 @@
+#frozen_string_literal: true
+
+INPUT_FILE = 'input.txt'
+
+calories_file = File.open(INPUT_FILE)
+calorie_data = calories_file.readlines.map(&:chomp)
+
+current_total_calories = 0
+largest_total_calories = 0
+
+calorie_data.each_with_index do |calorie|
+  unless calorie.empty?
+    current_total_calories += calorie.to_i
+  else
+    largest_total_calories = current_total_calories if current_total_calories > largest_total_calories
+    
+    current_total_calories = 0
+  end 
+end
+
+puts largest_total_calories
---
+++ 2022/Day1/p1b.rb
@@ -0,0 +1,23 @@
+#frozen_string_literal: true
+
+INPUT_FILE = 'input.txt'
+
+calories_file = File.open(INPUT_FILE)
+calorie_data = calories_file.readlines.map(&:chomp)
+
+current_total_calories = 0
+total_calories_per_elf = []
+
+calorie_data.each_with_index do |calorie|
+  unless calorie.empty?
+    current_total_calories += calorie.to_i
+  else
+    total_calories_per_elf.push(current_total_calories)
+
+    current_total_calories = 0
+  end 
+end
+
+total_calories_per_elf.push(current_total_calories)
+
+puts total_calories_per_elf.sort.reverse[0..2].sum
