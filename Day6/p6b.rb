dce8d6509cd47fb86d23e5c08494655cb8f51109
Steve Pursey <Stephenpursey@gmail.com>
2022-12-06 12:51:16 -0800

    Update var name

--- Day6/p6b.rb
+++ Day6/p6b.rb
@@ -6,20 +6,20 @@ file = File.open(INPUT_FILE)
 datastream = file.readlines.map(&:chomp).first
 
 char_counter = 0
-current_four_chars = []
+current_chars = []
 
 datastream.each_char do |char|
 
-  current_four_chars << char
+  current_chars << char
   char_counter += 1
 
-  next unless current_four_chars.length == 14
+  next unless current_chars.length == 14
 
-  if current_four_chars.uniq.length == 14
+  if current_chars.uniq.length == 14
     break
   end
 
-  current_four_chars.delete_at(0)
+  current_chars.delete_at(0)
 end
 
 puts char_counter
